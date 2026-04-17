cea7ea5d15a1f319a704f1a81d4f64ac3c40743a
vshe0007 <vshe0007@student.monash.edu>
2026-04-17 18:40:51 +1000

    bug(router): Fixed crashing reloads.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import AccessGateView from '@/views/AccessGateView.vue'
 import AwarenessView from '@/views/AwarenessView.vue'
 import ForecastView from '@/views/ForecastView.vue'
@@ -7,7 +7,7 @@ import HomeView from '@/views/HomeView.vue'
 const ACCESS_KEY = 'gb_access_granted'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/login',
